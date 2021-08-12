762ba659322c86c6aaad6e987b1b10e4c857d972
dshskf <alexkeman9@gmail.com>
2021-08-13 00:32:23 +0700

    last

---
+++ JS/ascii.js
@@ -0,0 +1,34 @@
+let asciiText = {
+    react:
+        `
+---------------------------
+ _____                 _   
+|  __ \\               | |  
+| |__) |___  __ _  ___| |_ 
+|  _  // _ \\/ _  |/ __| __|
+| | \\ \\  __/ (_| | (__| |_ 
+|_|  \\_\\___|\\____|\\___|\\__|
+
+---------------------------    
+`,
+    node: `
+--------------------------------------
+ _   _           _           _  _____ 
+| \\ | |         | |         | |/ ____|
+|  \\| | ___   __| | ___     | | (___  
+| .   |/ _ \\ / _  |/ _ \\_   | |\\___ \\ 
+| |\\  | (_) | (_| |  __/ |__| |____) |
+|_| \\_|\\___/ \\____|\\___|\\____/|_____/ 
+
+--------------------------------------
+`,
+
+}
+
+let skillList = {
+    frontend: ['Redux', 'Redux-Saga', 'Styled-components', 'Context API', 'Router', 'React Hooks', 'PWA'],
+    backend: ['GraphQL', 'Express', 'Socket.IO', 'Redis'],
+    android: ['React Native'],
+    database: ['MongoDB', 'PostgreSQL', 'MySQL'],
+    services: ['AWS EC2', 'Firebase Cloud Messaging', 'Firebase Realtime Database']
+}
---
+++ JS/main.js
@@ -0,0 +1,87 @@
+feather.replace()
+$(document).ready(function () {
+    (() => {
+        setTimeout(() => {
+            $('.section--welcome').hide()
+        }, 0)
+    })()
+
+    
+
+    let isFocus = false,
+        countPage = 1
+
+    $('.container').on('scroll', e => {
+        let position = e.target.scrollTop
+
+        console.log(window.innerHeight)
+
+    })
+})
+
+
+//                let appendText = () => {
+        //                    let words = "Welcome...",
+        //                        wordIndex = 0;
+        //                    let func = () => {
+        //                        $('.welcome-h1').html(words.substr(0, wordIndex))
+        //                        wordIndex += 1
+        //                        if (wordIndex > words.length) {
+        //                            clearInterval(ivl)
+        //                        }
+        //                    }
+        //
+        //                    let ivl = setInterval(func, 200)
+        //                    let clears = () => {
+        //                        clearInterval(ivl)
+        //                    }
+        //                }
+        //
+        //                setTimeout(() => {
+        //                    $('.section--welcome').hide()
+        //                }, 9500)
+
+    //            $(".about-card").click(() => {
+    // document.documentElement.style.setProperty('--blue', 'red');
+    // })
+    //            
+    // const section_about = $('.section--about'),
+    //     section_edu = $('.section--education'),
+    //     section_skill = $('.section--skill')
+
+    // let iPosition = 0,
+    //     iSkill = 0,
+    //     start = 820,
+    //     scrolled = false;
+
+    // $('.container').on('scroll', e => {
+    //     let position = e.target.scrollTop
+
+    //     // Main Panel   
+    //     if (Math.abs(iPosition) <= 45) {
+    //         // If more than Education
+    //         if (position > start) {
+    //             scrolled = false
+    //             iPosition = -(position - start) / 18;
+    //             $('.main-panel').show()
+    //         }
+
+    //         // If less than education and postion not 0
+    //         if (position < start && iPosition < 0) {
+    //             iPosition = 0
+    //         }
+
+    //         if (Math.abs(iPosition) > 45 && !scrolled) {
+    //             scrolled = true
+    //             $('.container').scrollTop(section_about.outerHeight() + section_edu.outerHeight() + 160)
+    //         }
+    //         $('.section--skills').css('transform', `translateX(${iPosition / 1.05}vw)`)
+    //         $('.main-panel').css('transform', `translateX(${iPosition}vw)`)
+    //     } else {
+    //         iPosition = -(position - start) / 18;
+    //         $('.section--skills').css('transform', `translateX(-45vw)`)
+    //         $('.main-panel').hide()
+    //     }
+    //     // Skill                
+
+    // })
