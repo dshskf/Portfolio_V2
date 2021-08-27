8c74b33af7d9636f06ab70fe627d9b212ea0de5b
dshskf <alexkeman9@gmail.com>
2021-08-27 22:43:02 +0700

    up

---
+++ JS/data.js
@@ -0,0 +1,120 @@
+const skillsData = {
+    frontend: [
+        {
+            title: 'HTML',
+            percentage: 13
+        },
+        {
+            title: 'SASS',
+            percentage: 10
+        },
+        {
+            title: 'JS',
+            percentage: 12
+        },
+        {
+            title: 'ReactJS',
+            percentage: 12
+        },
+        {
+            title: 'NextJS',
+            percentage: 10
+        },
+    ],
+    backend: [
+        {
+            title: 'NodeJS',
+            percentage: 12
+        },
+        {
+            title: 'Laravel',
+            percentage: 10
+        },
+        {
+            title: 'GraphQL',
+            percentage: 11
+        },
+        {
+            title: 'PostgreSQL',
+            percentage: 12
+        },
+        {
+            title: 'MySQL',
+            percentage: 12
+        },
+        {
+            title: 'MongoDB',
+            percentage: 9
+        }
+    ],
+    android: [
+        {
+            title: 'React Native',
+            percentage: 11
+        },
+        {
+            title: 'IONIC',
+            percentage: 9
+        }
+    ],
+    services: [
+        {
+            title: 'AWS',
+            percentage: 10
+        },
+        {
+            title: 'Firebase',
+            percentage: 10
+        },
+        {
+            title: 'Git',
+            percentage: 11
+        }
+    ],
+    editing: [
+        {
+            title: 'Photoshop',
+            percentage: 12
+        },
+        {
+            title: 'Figma',
+            percentage: 11
+        },
+        {
+            title: 'Premiere',
+            percentage: 9
+        }
+    ]
+}
+
+
+const experienceData = {
+    tbig: {
+        company: 'Tower Bersama Infrastructure',
+        title: 'Web Developer',
+        img: '',
+        project: ['IDI', 'SCANNER', 'Galang Bersama', 'EzyPolis'],
+        technology: ['Laravel', 'Github', 'Jira']
+    },
+    enseval: {
+        company: 'Enseval Putera Megatrading',
+        title: 'Fullstack Developer',
+        img: '',
+        project: ['GLOB'],
+        technology: ['ReactJS', 'NodeJS', 'Firebase', 'AWS', 'Redis', 'WebSocket', 'PostgreSQL']
+    },
+    idstar: {
+        company: 'IDStar Cipta Teknologi',
+        title: 'RPA Developer',
+        img: '',
+        project: ['Bank Mega', 'Bank BCA', 'Indosat'],
+        technology: ['UIPath']
+    },
+    bca: {
+        company: 'Bank Central Asia',
+        title: 'RPA Developer',
+        img: '',
+        project: ['Bank Mega', 'Bank BCA', 'Indosat'],
+        technology: ['UIPath']
+    }
+}
--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,125 @@
 $(document).ready(function () {
+    (function () {
+        for (var x = 0; x < 1; x++) {
+            console.log(x)
+        }
+    })()
+
+
+    let skillIndex = 0,
+        cardTracker = {}
+
+
+    $('.nav-link').click(function () {
+        // var link = document.createElement('a');
+        // link.href = 'https://drive.google.com/file/d/1YgnLGE6m-F8qMYK5EwONM0dqLooXJ2z3/view?usp=sharing';
+        // link.download = 'file.pdf';
+        // link.dispatchEvent(new MouseEvent('click'));
+        // $('.section--education').scrollIntoView();        
+        document.getElementById($(this).text().toLowerCase()).scrollIntoView({ behavior: "smooth" });
+
+    })
+
+
+    const handlerSkillsNav = num => {
+        if (skillIndex + num < 0 || skillIndex + num > Object.keys(skillsData).length - 1) {
+            return false
+        }
+
+        skillIndex += num
+
+        let newElement = ''
+        Object.keys(skillsData).forEach((key, i) => {
+            if (i === skillIndex) {
+                $('.skills-title h1').text(key[0].toUpperCase() + key.substring(1, key.length))
+
+                skillsData[key].forEach((skill, j) => {
+                    newElement += `
+                        <div class="sc-progress">                            
+                            <p>${skill.title}</p>              
+                            <div class="progress-bar bar-${skill.percentage}"></div>      
+                        </div>
+                    `
+                })
+            }
+        })
+        $('.skills-content').empty()
+        $('.skills-content').append(newElement)
+    }
+
     (() => {
-        // $('.about-skill').trigger('click');
+        handlerSkillsNav(0)
+    })()
+
+    $('.container').on('scroll', e => {
+        let position = e.target.scrollTop
+    })
+
+
+    let isAnimated = false
+    $('#btn-prev').click(function () {
+        if (!isAnimated) {
+            isAnimated = true
+        } else {
+            return false
+        }
+
+        $('.skills-content').css('transform', 'translateX(200px)')
+        $('.skills-content').css('filter', 'opacity(0)')
+
+        if (isAnimated) {
+            setTimeout(() => {
+                handlerSkillsNav(-1)
+                $('.skills-content').css('transform', 'translateX(0)')
+                $('.skills-content').css('filter', 'opacity(1)')
+
+                setTimeout(() => {
+                    isAnimated = false
+                }, 1000)
+            }, 1000)
+        }
+
+    })
+    $('#btn-next').click(function () {
+        if (!isAnimated) {
+            isAnimated = true
+        } else {
+            return false
+        }
+
+        $('.skills-content').css('transform', 'translateX(-200px)')
+        $('.skills-content').css('filter', 'opacity(0)')
+
+        if (isAnimated) {
+            setTimeout(() => {
+                handlerSkillsNav(1)
+                $('.skills-content').css('transform', 'translateX(0)')
+                $('.skills-content').css('filter', 'opacity(1)')
+
+                setTimeout(() => {
+                    isAnimated = false
+                }, 1000)
+            }, 1000)
+        }
+
+    })
+
+
+    $('.exp-card').click(function () {
+        const clickedCard = $(this).children('.company').val()
+        if (cardTracker[clickedCard]) {
+            cardTracker[clickedCard] = !cardTracker[clickedCard]
+            $(this).children('.ec-content').children('.ec-content-details').css('transform', 'translateY(-100%)')
+            return false
+        }
+
+        cardTracker[clickedCard] = true
+        $(this).children('.ec-content').children('.ec-content-details').css('transform', 'translateY(0)')
+    })
+})
+
+
+ // $('.about-skill').trigger('click');
         // let c = document.getElementById("education"),
         //     cWidth = c.getBoundingClientRect().width,
         //     cHeight = c.getBoundingClientRect().height
@@ -29,18 +148,7 @@ $(document).ready(function () {
         //     // ctx.fillText(charData[shuffle], random_w, random_h);
         //     ctx.stroke();
         // }
-    })()
 
-
-
-    let isFocus = false,
-        countPage = 1
-
-    $('.container').on('scroll', e => {
-        let position = e.target.scrollTop
-
-    })
-})
 
 
 //                let appendText = () => {
